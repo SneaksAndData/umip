59522d179f299ea7d36763b043a02537d9e31b3c
George Zubrienko <gzu@ecco.com>
2026-09-04 08:55:28 +0200

    [Copyright] Set license and ownership (#10)

--- src/umip/__init__.py
+++ src/umip/__init__.py
@@ -1,5 +1,33 @@
 """Init file."""
 
+#  Copyright (c) 2026. ECCO Data & AI and other project contributors.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 from umip.abstract_constr_builder import AbstractConstraintBuilder as AbstractConstraintBuilder
 from umip.abstract_data_prep import AbstractDataPreparator as AbstractDataPreparator
 from umip.abstract_mip import AbstractMipModel as AbstractMipModel
--- src/umip/solver_config/__init__.py
+++ src/umip/solver_config/__init__.py
@@ -1,5 +1,20 @@
 """Typed solver configuration objects."""
 
+#  Copyright (c) 2026. ECCO Data & AI and other project contributors.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 from umip.solver_config.base import SolverConfig as SolverConfig
 from umip.solver_config.cplex import CplexSolverConfig as CplexSolverConfig
 from umip.solver_config.gurobi import GurobiSolverConfig as GurobiSolverConfig
